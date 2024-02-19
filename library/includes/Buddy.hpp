a4f2bc380df2ac5d97a3501f29aba4217ca22b05
razerx100 <saikatmandal8@gmail.com>
2024-02-19 00:41:07 +0000

    Change AddAllocBlock's logic
    I am dumb

--- library/includes/Buddy.hpp
+++ library/includes/Buddy.hpp
@@ -130,9 +130,9 @@ private:
 	static void AddAllocBlock(
 		std::vector<AllocatorBase::AllocInfo<T>>& blocks, size_t startingAddress, size_t size
 	) noexcept {
-		// The blocks container should be sorted by size. So, let's use std::upper_bound to find
+		// The blocks container should be sorted by size. So, let's use std::lower_bound to find
 		// where to add this new block. So, the container stays sorted.
-		auto result = std::ranges::upper_bound(blocks, size, {},
+		auto result = std::ranges::lower_bound(blocks, size, {},
 			[](const AllocatorBase::AllocInfo<T>& element)
 			{
 				return element.size;
@@ -140,7 +140,7 @@ private:
 		);
 
 		// If no larger element exists, add the new block at the end. Otherwise,
-		// the result would be an element which is the next larger value from the given one.
+		// the result would be either equal to larger than the size.
 		// So, we should insert the new element before it.
 		if (result == std::end(blocks))
 			blocks.emplace_back(MakeAllocInfo<T>(startingAddress, size));
